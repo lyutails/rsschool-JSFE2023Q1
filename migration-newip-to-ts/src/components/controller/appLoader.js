0397d1b41e9c4244b75b025d6e8316547f5c4512
lyutails <lyusya.cool@gmail.com>
2023-06-07 21:56:01 +0300

    feat: install dependencies and add api key

--- migration-newip-to-ts/src/components/controller/appLoader.js
+++ migration-newip-to-ts/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'a381b3a9a2f3442b8c51e466153652fa', // получите свой ключ https://newsapi.org/
         });
     }
 }
