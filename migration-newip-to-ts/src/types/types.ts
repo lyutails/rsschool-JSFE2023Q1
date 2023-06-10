c85f0330fed81af0719a4bbc889345f378888cfb
lyutails <lyusya.cool@gmail.com>
2023-06-11 02:52:50 +0300

    fix: remove unused interface from type and create export all interfaces with index file

---
+++ migration-newip-to-ts/src/types/types.ts
@@ -0,0 +1,57 @@
+enum Status {
+  ok = 'ok',
+  error = 'error',
+}
+
+export enum ErrorCodes {
+  unauthorized = 401,
+  notFound = 404,
+}
+
+export type Options = {
+  [key: string]: string | number;
+};
+
+export type Endpoint = string;
+
+export type EndpointOptions = {
+  endpoint: Endpoint;
+  options?: Options;
+};
+
+export type Source = {
+  id: string;
+  name: string;
+  description?: string;
+  url?: string;
+  category?: string;
+  language?: string;
+  country?: string;
+};
+
+export interface SourcesResponse {
+  status: Status;
+  sources: Source[];
+}
+
+export interface NewsItem {
+  author: string;
+  content: string;
+  description: string;
+  publishedAt: string;
+  source: {
+    id: string;
+    name: string;
+  };
+  title: string;
+  url: string;
+  urlToImage: string;
+}
+
+export interface HeadlinesResponse {
+  status: Status;
+  totalResults: number;
+  articles: NewsItem[];
+}
+
+export type callbackFunction<T> = (data?: T) => void;
