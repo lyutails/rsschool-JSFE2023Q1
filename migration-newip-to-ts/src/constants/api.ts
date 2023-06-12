e84697f1731da82939a948a86991cf4a5062bcea
lyutails <lyusya.cool@gmail.com>
2023-06-12 23:28:05 +0300

    feat: change base url to the one working with localhost

--- migration-newip-to-ts/src/constants/api.ts
+++ migration-newip-to-ts/src/constants/api.ts
@@ -1,2 +1,2 @@
-// export const BASE_URL = 'https://newsapi.org/v2/';
-export const BASE_URL = 'https://rss-news-api.onrender.com/';
+export const BASE_URL = 'https://newsapi.org/v2/';
+// export const BASE_URL = 'https://rss-news-api.onrender.com/';
