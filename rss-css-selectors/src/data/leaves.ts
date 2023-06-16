a3274a0a4ab16a58ac2105a98c01409473c3cc3e
lyutails <lyusya.cool@gmail.com>
2023-06-16 22:09:31 +0300

    feat: create data folder and file

---
+++ rss-css-selectors/src/data/leaves.ts
@@ -0,0 +1,109 @@
+import { LeavesPics } from '../types/types';
+
+import leaf_01 from '@/refs/tree_night/leaf_01_night.png';
+import leaf_02 from '@/refs/tree_night/leaf_02_night.png';
+import leaf_03 from '@/refs/tree_night/leaf_03_night.png';
+import leaf_04 from '@/refs/tree_night/leaf_04_night.png';
+import leaf_05 from '@/refs/tree_night/leaf_05_night.png';
+import leaf_06 from '@/refs/tree_night/leaf_06_night.png';
+import leaf_07 from '@/refs/tree_night/leaf_07_night.png';
+import leaf_08 from '@/refs/tree_night/leaf_08_night.png';
+import leaf_09 from '@/refs/tree_night/leaf_09_night.png';
+import leaf_10 from '@/refs/tree_night/leaf_10_night.png';
+import leaf_11 from '@/refs/tree_night/leaf_11_night.png';
+import leaf_12 from '@/refs/tree_night/leaf_12_night.png';
+import leaf_13 from '@/refs/tree_night/leaf_13_night.png';
+import leaf_14 from '@/refs/tree_night/leaf_14_night.png';
+import leaf_15 from '@/refs/tree_night/leaf_15_night.png';
+import leaf_16 from '@/refs/tree_night/leaf_16_night.png';
+import leaf_17 from '@/refs/tree_night/leaf_17_night.png';
+import leaf_18 from '@/refs/tree_night/leaf_18_night.png';
+import leaf_19 from '@/refs/tree_night/leaf_19_night.png';
+import leaf_20 from '@/refs/tree_night/leaf_20_night.png';
+import leaf_21 from '@/refs/tree_night/leaf_21_night.png';
+import leaf_22 from '@/refs/tree_night/leaf_22_night.png';
+import leaf_23 from '@/refs/tree_night/leaf_23_night.png';
+import leaf_24 from '@/refs/tree_night/leaf_24_night.png';
+import leaf_25 from '@/refs/tree_night/leaf_25_night.png';
+import leaf_26 from '@/refs/tree_night/leaf_26_night.png';
+
+export const leavesArray: LeavesPics[] = [
+  {
+    '@/refs/tree_night/leaf_01_night.png': leaf_01
+  },
+  {
+    '@/refs/tree_night/leaf_02_night.png': leaf_02
+  },
+  {
+    '@/refs/tree_night/leaf_03_night.png': leaf_03
+  },
+  {
+    '@/refs/tree_night/leaf_04_night.png': leaf_04
+  },
+  {
+    '@/refs/tree_night/leaf_05_night.png': leaf_05
+  },
+  {
+    '@/refs/tree_night/leaf_06_night.png': leaf_06
+  },
+  {
+    '@/refs/tree_night/leaf_07_night.png': leaf_07
+  },
+  {
+    '@/refs/tree_night/leaf_08_night.png': leaf_08
+  },
+  {
+    '@/refs/tree_night/leaf_09_night.png': leaf_09
+  },
+  {
+    '@/refs/tree_night/leaf_10_night.png': leaf_10
+  },
+  {
+    '@/refs/tree_night/leaf_11_night.png': leaf_11
+  },
+  {
+    '@/refs/tree_night/leaf_12_night.png': leaf_12
+  },
+  {
+    '@/refs/tree_night/leaf_13_night.png': leaf_13
+  },
+  {
+    '@/refs/tree_night/leaf_14_night.png': leaf_14
+  },
+  {
+    '@/refs/tree_night/leaf_15_night.png': leaf_15
+  },
+  {
+    '@/refs/tree_night/leaf_16_night.png': leaf_16
+  },
+  {
+    '@/refs/tree_night/leaf_17_night.png': leaf_17
+  },
+  {
+    '@/refs/tree_night/leaf_18_night.png': leaf_18
+  },
+  {
+    '@/refs/tree_night/leaf_19_night.png': leaf_19
+  },
+  {
+    '@/refs/tree_night/leaf_20_night.png': leaf_20
+  },
+  {
+    '@/refs/tree_night/leaf_21_night.png': leaf_21
+  },
+  {
+    '@/refs/tree_night/leaf_22_night.png': leaf_22
+  },
+  {
+    '@/refs/tree_night/leaf_23_night.png': leaf_23
+  },
+  {
+    '@/refs/tree_night/leaf_24_night.png': leaf_24
+  },
+  {
+    '@/refs/tree_night/leaf_25_night.png': leaf_25
+  },
+  {
+    '@/refs/tree_night/leaf_26_night.png': leaf_26
+  }
+];
