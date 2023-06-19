5043339383b1ae85504bd41bb3f483fcf3f40ba9
lyutails <lyusya.cool@gmail.com>
2023-06-19 03:37:24 +0300

    feat: add eye switcher for hints visibility

--- rss-css-selectors/src/components/hints/hints.ts
+++ rss-css-selectors/src/components/hints/hints.ts
@@ -6,5 +6,19 @@ export class Hints extends BaseComponent<'div'> {
       tagName: 'div',
       classList: ['hints']
     });
+
+    const text = new BaseComponent({
+      tagName: 'div',
+      classList: ['hints_text']
+    });
+
+    const eyeVisibility = new BaseComponent({
+      tagName: 'div',
+      classList: ['hints_eye']
+    });
+
+    eyeVisibility.toggleClassOnClick();
+
+    this.node.append(eyeVisibility.node, text.node);
   }
 }
