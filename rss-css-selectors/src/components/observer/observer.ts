7c23a718103adb18256cfc8ac1eb3375f3ff1d16
lyutails <lyusya.cool@gmail.com>
2023-06-23 18:20:49 +0300

    feat: create observer

---
+++ rss-css-selectors/src/components/observer/observer.ts
@@ -0,0 +1,19 @@
+import { Subscriber } from '../../types/types';
+
+export class Observer<T> {
+  public observers: Subscriber<T>[] = [];
+
+  public subscribe(observerFunction: Subscriber<T>): void {
+    this.observers.push(observerFunction);
+  }
+
+  public unsubscribe(observerFunction: Subscriber<T>): void {
+    this.observers = this.observers.filter((fn) => fn !== observerFunction);
+  }
+
+  public notify(data: T): void {
+    this.observers.forEach((observer) => {
+      observer(data);
+    });
+  }
+}
